c6e856c22df13be0811268506dc0c846679e703d
Abhinraj-ss <abhinrajss18@gmail.com>
2021-06-20 16:36:09 +0530

    added features and icons

--- app/src/main/java/com/abhinraj/birthdayremainder/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/abhinraj/birthdayremainder/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Upcoming Birthdays"
     }
     val text: LiveData<String> = _text
 }
